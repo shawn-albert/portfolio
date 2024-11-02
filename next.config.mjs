8583afda64925a0a7efd6d78faff13eee966d26e
Shawn Albert <shawn.c.albert@gmail.com>
2024-11-02 20:00:47 +0000

    fix(rm-static-exports): remove static export from configuration to patch pnpm build

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,16 @@ import { withContentCollections } from '@content-collections/next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   experimental: {
     serverActions: {
-      allowedOrigins: ['localhost:3000']
-    }
+      allowedOrigins: ['localhost:3000'],
+    },
   },
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: {
-    unoptimized: true
-  }
+    unoptimized: true,
+  },
 };
 
 export default withContentCollections(nextConfig);
