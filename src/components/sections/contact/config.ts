94c2df7e207c7d64f6c1e6f465ab2ac0ab54e535
Shawn Albert <shawn.c.albert@gmail.com>
2024-11-02 21:01:20 +0000

    chore(social-links): update social links to point to profiles; comment out YouTube and Twitter

--- src/components/sections/contact/config.ts
+++ src/components/sections/contact/config.ts
@@ -2,31 +2,31 @@ import { Contact } from '@/types/contact';
 import {
   SiGithub,
   SiLinkedin,
-  SiX,
-  SiYoutube
+  // SiX,
+  // SiYoutube
 } from '@icons-pack/react-simple-icons';
 
 const contact: Contact = {
-  email: 'john@example.com',
+  email: 'contact@shawn-albert.com',
   socials: [
     {
       name: 'Github',
-      href: 'https://github.com/',
+      href: 'https://github.com/shawn-albert',
       Icon: SiGithub
     },
-    {
-      name: 'Youtube',
-      href: 'https://youtube.com/',
-      Icon: SiYoutube
-    },
-    {
-      name: 'X',
-      href: 'https://x.com/',
-      Icon: SiX
-    },
+    // {
+    //   name: 'Youtube',
+    //   href: 'https://youtube.com/',
+    //   Icon: SiYoutube
+    // },
+    // {
+    //   name: 'X',
+    //   href: 'https://x.com/',
+    //   Icon: SiX
+    // },
     {
       name: 'Linkedin',
-      href: 'https://linkedin.com/',
+      href: 'https://www.linkedin.com/in/shawnalbert/',
       Icon: SiLinkedin
     }
   ]
